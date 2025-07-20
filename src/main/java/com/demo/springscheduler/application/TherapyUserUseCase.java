22b1ca82758376097e8e1b696efd57b26f372ee7
hwangdaesun <hds650588@gmail.com>
2025-07-20 15:42:51 +0900

    feat : 치료를 수행하는 메서드 생성

--- src/main/java/com/demo/springscheduler/application/TherapyUserUseCase.java
+++ src/main/java/com/demo/springscheduler/application/TherapyUserUseCase.java
@@ -12,20 +12,30 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class TherapyUserUseCase {
+    private final TherapyItemReader therapyItemReader;
     private final TherapyUserReader therapyUserReader;
     private final TherapyUserStore therapyUserStore;
     private final TherapyPlanStore therapyPlanStore;
+    private final TherapyPerformStore therapyPerformStore;
 
     @Transactional
     public void createTherapyUser(String email) {
         TherapyUser therapyUser = TherapyUser.create(email);
         therapyUserStore.store(therapyUser);
     }
-
     @Transactional
     public void assignTherapyItemsToUser(String email, List<TherapyItem> therapyItems) {
         TherapyUser therapyUser = therapyUserReader.read(email);
         List<TherapyPlan> therapyPlans = TherapyPlan.assignItemToUser(therapyUser, therapyItems);
         therapyPlanStore.saveAll(therapyPlans);
+    }
+
+    @Transactional
+    public void performTherapy(String email, Long therapyItemId, Float someData1, Float someData2){
+        TherapyUser therapyUser = therapyUserReader.read(email);
+        // 사용자 therapyPlan 인지 검증
+        TherapyItem therapyItem = therapyItemReader.read(therapyItemId);
+        TherapyPerform therapyPerform = TherapyPerform.perform(therapyUser, therapyItem, someData1, someData2);
+        therapyPerformStore.store(therapyPerform);
     }
 }
