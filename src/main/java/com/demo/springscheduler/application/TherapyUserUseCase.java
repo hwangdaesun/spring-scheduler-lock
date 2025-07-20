177f7d4ff0ac3cfceaf9541216c240dd9136ea47
hwangdaesun <hds650588@gmail.com>
2025-07-20 15:42:25 +0900

    feat : 치료 항목을 사용자에게 할당하는 메서드 생성

--- src/main/java/com/demo/springscheduler/application/TherapyUserUseCase.java
+++ src/main/java/com/demo/springscheduler/application/TherapyUserUseCase.java
@@ -12,11 +12,20 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class TherapyUserUseCase {
+    private final TherapyUserReader therapyUserReader;
     private final TherapyUserStore therapyUserStore;
+    private final TherapyPlanStore therapyPlanStore;
 
     @Transactional
     public void createTherapyUser(String email) {
         TherapyUser therapyUser = TherapyUser.create(email);
         therapyUserStore.store(therapyUser);
+    }
+
+    @Transactional
+    public void assignTherapyItemsToUser(String email, List<TherapyItem> therapyItems) {
+        TherapyUser therapyUser = therapyUserReader.read(email);
+        List<TherapyPlan> therapyPlans = TherapyPlan.assignItemToUser(therapyUser, therapyItems);
+        therapyPlanStore.saveAll(therapyPlans);
     }
 }
