8e8ffe6738bbdcc41711266c909deee3dab5e51f
dustin.hwang <dustin.hwang@turingbio.com>
2025-07-26 12:07:36 +0900

    feat : NamedLockRepository 생성

---
+++ src/main/java/com/demo/springscheduler/infra/scheduler/lock/NamedLockRepository.java
@@ -0,0 +1,22 @@
+package com.demo.springscheduler.infra.scheduler.lock;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class NamedLockRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public boolean acquireLock(String lockName) {
+        String sql = "SELECT GET_LOCK(?, 1)";
+        Boolean result = jdbcTemplate.queryForObject(sql, Boolean.class, lockName);
+        return Boolean.TRUE.equals(result);
+    }
+
+    public void releaseLock(String lockName) {
+        jdbcTemplate.update("SELECT RELEASE_LOCK(?)", lockName);
+    }
+}
