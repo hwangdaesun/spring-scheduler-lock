839466d9540502b8f32c2138a7c1f185e039d7f0
dustin.hwang <dustin.hwang@turingbio.com>
2025-07-26 12:08:39 +0900

    test : Named Lock을 사용해서 2개 이상의 애플리케이션이 동시에 같은 param으로 insert할 경우 예외가 발생하는 지 확인하느 테스트 추가

--- src/test/java/com/demo/springscheduler/infra/scheduler/TherapyStatisticsServiceTest.java
+++ src/test/java/com/demo/springscheduler/infra/scheduler/TherapyStatisticsServiceTest.java
@@ -9,6 +9,7 @@ import com.demo.springscheduler.application.TherapyStatisticsReader;
 import com.demo.springscheduler.application.TherapyUserReader;
 import com.demo.springscheduler.domain.therapy.TherapyCalculator;
 import com.demo.springscheduler.domain.user.TherapyUser;
+import com.demo.springscheduler.infra.scheduler.lock.NamedLockRepository;
 import java.time.LocalDateTime;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -45,6 +46,9 @@ class TherapyStatisticsServiceTest {
     private TherapyPerformReader therapyPerformReader;
     @Autowired
     private TherapyCalculator therapyCalculator;
+
+    @Autowired
+    private NamedLockRepository namedLockRepository;
 
     @BeforeEach
     void beforeEach() {
@@ -84,6 +88,43 @@ class TherapyStatisticsServiceTest {
 
         // 예외가 1개 이상 발생해야 테스트 통과
         assertThat(exceptionCount.get()).isGreaterThan(0);
+    }
+
+    @Test
+    void concurrent_aggregation_should_success_with_lock() throws InterruptedException {
+        TherapyUser therapyUser = therapyUserReader.read(1L);
+        LocalDateTime start = LocalDateTime.now().minusDays(10);
+        LocalDateTime end = LocalDateTime.now().plusDays(10);
+
+        int threadCount = 3;
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        AtomicInteger exceptionCount = new AtomicInteger(0);
+
+        for (int i = 0; i < threadCount; i++) {
+            executor.submit(() -> {
+                namedLockRepository.acquireLock("batch-lock");
+                TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+                try {
+                    therapyStatisticsService.aggregateTherapyStatics(therapyUser.getId(), start, end);
+                    transactionManager.commit(status);
+                } catch (DataIntegrityViolationException e) {
+                    exceptionCount.incrementAndGet();
+                    transactionManager.rollback(status);
+                } finally {
+                    latch.countDown();
+                    namedLockRepository.releaseLock("batch-lock");
+                }
+            });
+        }
+
+        latch.await();
+        executor.shutdown();
+
+        System.out.println("예외 발생 횟수: " + exceptionCount.get());
+
+        // 예외가 발생하지 않아야 통과
+        assertThat(exceptionCount.get()).isEqualTo(0);
     }
 
 }
